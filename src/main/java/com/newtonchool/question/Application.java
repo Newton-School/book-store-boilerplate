c3931cef10b45e260ac9403c12c8f1074aae13a8
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-04-29 13:19:50 +0530

    Boilerplate done - Book Store

---
+++ src/main/java/com/newtonchool/question/Application.java
@@ -0,0 +1,27 @@
+package com.newtonschool.question;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Application {
+    public static List<String[]> readFromFile(String fileName) {
+        List<String[]> books = new ArrayList<>();
+        
+        return books;
+    }
+
+    // Method to calculate the average price of books
+    public static double calculateAveragePrice(List<String[]> books) {
+       
+    }
+
+    // Example main method to use the functions
+    public static void main(String[] args) {
+        List<String[]> books = readFromFile("dummy.txt");
+        double averagePrice = calculateAveragePrice(books);
+        System.out.println("Average Price: " + averagePrice);
+    }
+}
